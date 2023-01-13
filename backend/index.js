295ea7e5440e4bee0322b01737c10a5a4f716ba1
William <william@zenneatech.com>
2023-01-12 21:51:14 -0800

    Added user creation POST method

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
 const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json())
@@ -17,6 +18,7 @@ mongoose
   .catch((err) => console.log(err));
 
   app.use("/api/pins", pinRoute);
+  app.use("/api/users", userRoute);
 
 
 app.listen(8800, () => {
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,29 +1,49 @@
-const router = require("express").Router();
 const User = require("../models/User");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-// Register a new user
+// REGISTER
 router.post("/register", async (req, res) => {
-    const newPin = new Pin(req.body);
-    try {
-        // Generate new password
+  try {
+    // Generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        // Create new user
-        
-        // Save user and send response
-        
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
+    // Create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
-// Login to an existing user
-router.get("/", async(req, res)=>{
-    try {
-        const pins = await Pin.find();
-        res.status(200).json(pins);
-    } catch (error) {
-        res.status(500).json(error);
-    }
+    // Save user and response
+    const user = await newUser.save();
+    res.status(200).json(user._id);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-module.exports = router
+// //LOGIN
+// router.post("/login", async (req, res) => {
+//   try {
+//     // Generate new password
+//     const user = await User.findOne({ username: req.body.username });
+//     !user && res.status(400).json("Wrong username or password");
+
+//     // Validate password
+//     const validPassword = await bcrypt.compare(
+//       req.body.password,
+//       user.password
+//     );
+//     !validPassword && res.status(400).json("Wrong username or password");
+
+//     // Send response
+//     res.status(200).json({ _id: user._id, username: user.username });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+module.exports = router;
