665daa908708395f8940d9328c00230bbd224fac
William <william@zenneatech.com>
2023-01-15 22:23:53 -0800

    Added in Mapbox GL JS for frontend map view, working on pin functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
+
+import * as React from 'react';
+import {render} from 'react-dom';
+import Map, {Marker} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX; // Set your mapbox token here
+
 function App() {
   return (
-    <div className="App">
-    hello
-    </div>
+    <Map
+      initialViewState={{
+        latitude: 46.8,
+        longitude: 17.4,
+        zoom: 4
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v12"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      <Marker longitude={17.4} latitude={46.8} color="red" />
+    </Map>
   );
 }
+
+render(<App />, document.body.appendChild(document.createElement('div')));
 
 export default App;
