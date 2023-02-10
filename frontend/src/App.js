ffc99cbdf9740c5cbb661221627ae155a0f65a65
WilliamZhang0822 <william.zhang609@gmail.com>
2023-02-10 16:31:29 +0100

    Added in styling for popups, added in card element that represents reviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 
 import * as React from 'react';
 import {render} from 'react-dom';
-import Map, {Marker} from 'react-map-gl';
+import Map, {Marker, Popup} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { Star } from '@material-ui/icons';
+import "./App.css";
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX; // Set your mapbox token here
 
@@ -18,7 +20,34 @@ function App() {
       mapStyle="mapbox://styles/mapbox/streets-v12"
       mapboxAccessToken={MAPBOX_TOKEN}
     >
-      <Marker longitude={17.4} latitude={46.8} color="red" />
+      <Marker
+        latitude={46.8}
+        longitude={17.4}
+        color="red"
+      >
+      </Marker>
+      <Popup 
+        latitude={46.8}
+        longitude={17.4} 
+        anchor="left">
+        <div className='card'>
+          <label>Place</label>
+          <h4 className='place'><b>Berlin Wall</b></h4>
+          <label>Review</label>
+          <p className='description'>Beautiful place. I loved visiting.</p>
+          <label>Rating</label>
+          <div className='stars'>
+            <Star className='star'/>
+            <Star className='star'/>
+            <Star className='star'/>
+            <Star className='star'/>
+            <Star className='star'/>
+          </div>
+          <label>Information</label>
+          <span className='username'>Created by <b>William</b></span>
+          <span className='timestamp'>1 hour ago</span>
+        </div>
+      </Popup>
     </Map>
   );
 }
